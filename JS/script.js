decab2d5914cd097e9198f3a84442f0d2ebf515d
isma-91 <isma91@hotmail.it>
2022-10-21 17:11:35 +0200

    Fixed JS calcs

--- JS/script.js
+++ JS/script.js
@@ -3,31 +3,31 @@
 
 // let age = prompt("Quanti anni hai?");
 // age = parseInt(age);
-const priceKm = 0.21;
-
-let Name = document.querySelector("[name=name]").value;
-
-let Km = document.querySelector("[name=kilometres]").value;
-Km = parseFloat(Km);
-
-let age = document.querySelector("[name=yo]").value;
-age = parseInt(age);
-
 const eleConfirm = document.querySelector(".confirm");
 
-// Prezzo totale senza sconto (per età dai 19 ai 65)
-let totalPriceKm = priceKm * Km;
-totalPriceKm = parseFloat(totalPriceKm.toFixed(2));
-
-// Prezzo totale per i minorenni
-let totalPriceMin = totalPriceKm - totalPriceKm * 0.2;
-totalPriceMin = parseFloat(totalPriceMin.toFixed(2));
-
-// Prezzo totale per gli over 65
-let totalPriceOver = totalPriceKm - totalPriceKm * 0.4;
-totalPriceOver = parseFloat(totalPriceOver.toFixed(2));
-
 eleConfirm.addEventListener("click", function () {
+  const priceKm = 0.21;
+
+  let Name = document.querySelector("[name=name]").value;
+
+  let Km = document.querySelector("[name=kilometres]").value;
+  Km = parseFloat(Km);
+
+  let age = document.querySelector("[name=yo]").value;
+  age = parseInt(age);
+
+  // Prezzo totale senza sconto (per età dai 19 ai 65)
+  let totalPriceKm = priceKm * Km;
+  totalPriceKm = parseFloat(totalPriceKm.toFixed(2));
+
+  // Prezzo totale per i minorenni
+  let totalPriceMin = totalPriceKm - totalPriceKm * 0.2;
+  totalPriceMin = parseFloat(totalPriceMin.toFixed(2));
+
+  // Prezzo totale per gli over 65
+  let totalPriceOver = totalPriceKm - totalPriceKm * 0.4;
+  totalPriceOver = parseFloat(totalPriceOver.toFixed(2));
+
   if (age === "under") {
     console.log(`Il costo del tuo biglietto è: ${totalPriceMin}€`);
   } else if (age === "over") {
